f244c25939aa12e8079b64a1bb4ecd7b7af08a8a
ezjanos <oskar.jansson@ericsson.com>
2021-04-23 15:09:03 +0200

    updates: install&requirements

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ python3 -m venv .venv &&
 echo "                                 done <-" &&
 source .venv/bin/activate &&
 echo "installing requirements ->" &&
-pip3 install --upgrade pip 1>/dev/null &&
-pip3 install -r src/requirements.txt 1>/dev/null &&
+pip3 install --upgrade pip 1> /dev/null &&
+pip3 install -r requirements.txt 1> /dev/null &&
 deactivate &&
 echo "                   done <-"
 echo -e "activate on linux with \nsource .venv/bin/activate"
--- src/cccpchart.py
+++ src/cccpchart.py
@@ -5,407 +5,407 @@ class CccpChart(BaseChart):
     type = ChartType.Bar
 
     class labels:
-        grouped = ['April21', 'April22']
+        grouped = ['April21', 'April22', 'April23']
 
     class data:
 
         class Btc:
-            data = [373, 694]
+            data = [373, 694, 726]
             backgroundColor = Color.Hex("#F7931A")
 
         class Eth:
-            data = [561, 1515]
+            data = [561, 1515, 528]
             backgroundColor = Color.Hex("#3c3c3d")
 
         class Bnb:
-            data = [216, 85]
+            data = [216, 85, 50]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xrp:
-            data = [29, 31]
+            data = [29, 31, 36]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Doge:
-            data = [332, 271]
+            data = [332, 271, 334]
             backgroundColor = Color.Hex("#d9bd62")
 
         class Usdt:
-            data = [12, 19]
+            data = [12, 19, 34]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ada:
-            data = [120, 124]
+            data = [120, 124, 152]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Dot:
-            data = [31, 35]
+            data = [31, 35, 40]
             backgroundColor = Color.Hex("#e6007a")
 
         class Bch:
-            data = [8, 2]
+            data = [8, 2, 4]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ltc:
-            data = [27, 37]
+            data = [27, 37, 25]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Vet:
-            data = [395, 303]
+            data = [395, 303, 453]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Uni:
-            data = [8, 55]
+            data = [8, 55, 10]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Link:
-            data = [55, 63]
+            data = [55, 63, 53]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xlm:
-            data = [52, 43]
+            data = [52, 43, 81]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Usdc:
-            data = [13, 8]
+            data = [13, 8, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Theta:
-            data = [4, 4]
+            data = [4, 4, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Fil:
-            data = [0, 2]
+            data = [0, 2, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Trx:
-            data = [4, 0]
+            data = [4, 0, 4]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Wbtc:
-            data = [1, 0]
+            data = [1, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Sol:
-            data = [5, 27]
+            data = [5, 27, 15]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Neo:
-            data = [5, 9]
+            data = [5, 9, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Busd:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xmr:
-            data = [17, 6]
+            data = [17, 6, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Klay:
-            data = [0, 2]
+            data = [0, 2, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Eos:
-            data = [1, 1]
+            data = [1, 1, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Miota:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Btt:
-            data = [6, 2]
+            data = [6, 2, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Bsv:
-            data = [0, 0]
+            data = [0, 0, 2]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Lunda:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Cro:
-            data = [4, 11]
+            data = [4, 11, 7]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Atom:
-            data = [33, 34]
+            data = [33, 34, 34]
             backgroundColor = Color.Hex("#5064fb")
 
         class Okb:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Aave:
-            data = [3, 12]
+            data = [3, 12, 4]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ftt:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xtz:
-            data = [0, 6]
+            data = [0, 6, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Etc:
-            data = [21, 21]
+            data = [21, 21, 22]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ceth:
-            data = [0, 1]
+            data = [0, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Algo:
-            data = [102, 127]
+            data = [102, 127, 140]
             backgroundColor = Color.Hex("#000000")
 
         class Cusdc:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Cake:
-            data = [79, 55]
+            data = [79, 55, 42]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Avax:
-            data = [0, 0]
+            data = [0, 0, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xem:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Dai:
-            data = [4, 7]
+            data = [4, 7, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ht:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Rune:
-            data = [5, 6]
+            data = [5, 6, 4]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ksm:
-            data = [1, 3]
+            data = [1, 3, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Mkr:
-            data = [2, 31]
+            data = [2, 31, 5]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Egld:
-            data = [1, 1]
+            data = [1, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Dash:
-            data = [2, 3]
+            data = [2, 3, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Hot:
-            data = [23, 16]
+            data = [23, 16, 23]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Cdai:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Chz:
-            data = [15, 4]
+            data = [15, 4, 2]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Dcr:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Zec:
-            data = [2, 0]
+            data = [2, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Stx:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Snx:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Hbar:
-            data = [1, 2]
+            data = [1, 2, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Comp:
-            data = [1, 8]
+            data = [1, 8, 5]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Enj:
-            data = [17, 8]
+            data = [17, 8, 15]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Cel:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Zil:
-            data = [7, 13]
+            data = [7, 13, 7]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Leo:
-            data = [0, 1]
+            data = [0, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Sc:
-            data = [0, 2]
+            data = [0, 2, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Grt:
-            data = [7, 4]
+            data = [7, 4, 5]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Bat:
-            data = [32, 18]
+            data = [32, 18, 24]
             backgroundColor = Color.Hex("#fe5025")
 
         class Nexo:
-            data = [3, 1]
+            data = [3, 1, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Amp:
-            data = [4, 3]
+            data = [4, 3, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Dgb:
-            data = [5, 5]
+            data = [5, 5, 9]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ust:
-            data = [1, 1]
+            data = [1, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Sushi:
-            data = [7, 8]
+            data = [7, 8, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Qtum:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Near:
-            data = [25, 40]
+            data = [25, 40, 44]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Yfi:
-            data = [0, 1]
+            data = [0, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Matic:
-            data = [14, 76]
+            data = [14, 76, 15]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Mana:
-            data = [15, 4]
+            data = [15, 4, 5]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ont:
-            data = [2, 0]
+            data = [2, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Uma:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Btg:
-            data = [0, 0]
+            data = [0, 0, 6]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Rvn:
-            data = [4, 0]
+            data = [4, 0, 3]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Hbtc:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Fei:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Waves:
-            data = [5, 1]
+            data = [5, 1, 2]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Icx:
-            data = [6, 2]
+            data = [6, 2, 2]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Zrx:
-            data = [1, 0]
+            data = [1, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Iost:
-            data = [0, 2]
+            data = [0, 2, 2]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Wrx:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Flow:
-            data = [3, 11]
+            data = [3, 11, 9]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Lusd:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Nano:
-            data = [67, 56]
+            data = [67, 56, 38]
             backgroundColor = Color.Hex("#4A90E2")
 
         class Bnt:
-            data = [1, 1]
+            data = [1, 1, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Xdc:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Rsr:
-            data = [2, 3]
+            data = [2, 3, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class One:
-            data = [462, 479]
+            data = [462, 479, 539]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Zen:
-            data = [2, 1]
+            data = [2, 1, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Omg:
-            data = [6, 9]
+            data = [6, 9, 11]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ankr:
-            data = [9, 11]
+            data = [9, 11, 10]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Pax:
-            data = [0, 0]
+            data = [0, 0, 1]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Omi:
-            data = [5, 2]
+            data = [5, 2, 4]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Hnt:
-            data = [0, 0]
+            data = [0, 0, 0]
             backgroundColor = Color.Hex("#d8d8d8")
 
         class Ftm:
-            data = [2, 7]
+            data = [2, 7, 8]
             backgroundColor = Color.Hex("#d8d8d8")
 
