d298a889493498d60303d4f57c4d7702a130f22e
ezjanos <oskar.jansson@ericsson.com>
2021-04-23 14:40:04 +0200

    updates: install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 echo "creating virtual environment in .venv ->" &&
 python3 -m venv .venv &&
-echo "                                 done ->" &&
+echo "                                 done <-" &&
 source .venv/bin/activate &&
 echo "installing requirements ->" &&
-pip3 install -r src/requirements.txt &&
+pip3 install --upgrade pip 1>/dev/null &&
+pip3 install -r src/requirements.txt 1>/dev/null &&
 deactivate &&
 echo "                   done <-"
+echo -e "activate on linux with \nsource .venv/bin/activate"
